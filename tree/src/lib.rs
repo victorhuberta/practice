c8091e90f32269d1223d7be29d4e454029bb3345
Victor Huberta <victorhuberta@gmail.com>
2018-05-01 17:48:17 +0700

    Remove map_bf. Apparently it is more suitable for searching rather than mapping.

--- tree/src/lib.rs
+++ tree/src/lib.rs
@@ -7,8 +7,7 @@ pub trait Operable {
     fn lookup(&self, key: &Self::Key) -> Option<&Self::Val>;
     fn insert(self, key: Self::Key, val: Self::Val) -> Self;
     fn remove(self, key: &Self::Key) -> Self;
-    fn map_dfs<F: Fn(Self::Val) -> Self::Val>(self, f: Rc<F>) -> Self;
-    fn map_bfs<F: Fn(Self::Val) -> Self::Val>(self, f: Rc<F>) -> Self;
+    fn map_df<F: Fn(Self::Val) -> Self::Val>(self, f: Rc<F>) -> Self;
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -92,33 +91,14 @@ impl<K: PartialEq + PartialOrd, V> Operable for BinaryTree<K, V> {
         }
     }
 
-    fn map_dfs<F: Fn(Self::Val) -> Self::Val>(self, f: Rc<F>) -> Self {
+    fn map_df<F: Fn(Self::Val) -> Self::Val>(self, f: Rc<F>) -> Self {
         match self {
             Leaf => self,
             NonLeaf { key, val, left, right } => NonLeaf {
                 key,
                 val: f(val),
-                left: Box::new(left.map_dfs(f.clone())),
-                right: Box::new(right.map_dfs(f.clone()))
-            }
-        }
-    }
-
-    fn map_bfs<F: Fn(Self::Val) -> Self::Val>(self, q: [Self], f: Rc<F>) -> Self {
-        if q.len() == 0 && let NonLeaf { .. } = self {
-            q.push(self);
-        } else if let Leaf = self {
-            self
-        }
-
-        if q.len() > 0 {
-            let front = q.remove(0);
-            match front {
-                Leaf => front,
-                NonLeaf { key, val, left, right } => {
-                    q.push(left);
-                    q.push(right);
-                }
+                left: Box::new(left.map_df(f.clone())),
+                right: Box::new(right.map_df(f.clone()))
             }
         }
     }
@@ -284,7 +264,7 @@ mod tests {
     }
 
     #[test]
-    fn double_tree_vals_with_map_dfs() {
+    fn double_tree_vals_with_map_df() {
         let tree = NonLeaf {
             key: 5,
             val: 2,
@@ -318,44 +298,6 @@ mod tests {
             right: Box::new(Leaf)
         };
 
-        assert_eq!(tree.map_dfs(Rc::new(|x| x*2)), expected);
-    }
-
-    #[test]
-    fn triple_tree_vals_with_map_bfs() {
-        let tree = NonLeaf {
-            key: 5,
-            val: 2,
-            left: Box::new(NonLeaf {
-                key: 1,
-                val: 3,
-                left: Box::new(Leaf),
-                right: Box::new(NonLeaf {
-                    key: 3,
-                    val: 4,
-                    left: Box::new(Leaf),
-                    right: Box::new(Leaf)
-                })
-            }),
-            right: Box::new(Leaf)
-        };
-        let expected = NonLeaf {
-            key: 5,
-            val: 6,
-            left: Box::new(NonLeaf {
-                key: 1,
-                val: 9,
-                left: Box::new(Leaf),
-                right: Box::new(NonLeaf {
-                    key: 3,
-                    val: 12,
-                    left: Box::new(Leaf),
-                    right: Box::new(Leaf)
-                })
-            }),
-            right: Box::new(Leaf)
-        };
-
-        assert_eq!(tree.map_bfs(Rc::new(|x| x*3)), expected);
+        assert_eq!(tree.map_df(Rc::new(|x| x*2)), expected);
     }
 }
