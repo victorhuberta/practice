d04d0bee567744bb6c6011ebf9dc57dce40aaa49
Victor Huberta <victorhuberta@gmail.com>
2018-06-28 22:52:01 +0700

    Configure test with standalone selenium server.

--- selenium/test.js
+++ selenium/test.js
@@ -1,7 +1,9 @@
 var webdriver = require('selenium-webdriver');
 
-var driver = new webdriver.Builder().
-  withCapabilities(webdriver.Capabilities.chrome()).build();
+var driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .usingServer('http://localhost:4444/wd/hub')
+  .build();
 
 driver.get('http://www.google.com');
 driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
